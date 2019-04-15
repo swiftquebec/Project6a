03aca5aef99db9ac202bbdafc13d0f3fbcc11e6d
Gregory Leck <greg@swiftquebec.com>
2019-04-15 13:52:39 -0400

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Gregory Leck on 2019-04-15.
+//  Copyright © 2019 Gregory Leck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
